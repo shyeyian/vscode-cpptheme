d859265d25794c295059a72b0d8a610e9e76048a
anonymous <shyeyian@petalmail.com>
2025-12-22 10:03:49 +0800

    update

--- upload.js
+++ upload.js
@@ -2,6 +2,7 @@ let child_process = require("child_process")
 let fs = require("fs")
 
 try {
+    child_process.execSync("git pull")
     child_process.execSync("git add .")
     child_process.execSync("git commit -m update")
 }
