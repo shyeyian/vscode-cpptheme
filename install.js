761524a1349daba1ef3b9ce98f6452e0a4a74e33
shyeyian <shyeyian@icloud.com>
2026-02-23 16:32:43 +0800

    update

--- install.js
+++ install.js
@@ -1,14 +1,20 @@
+// @ts-check
+
 const child_process = require('child_process')
 const fs            = require('fs')
 const util          = require('util')
-child_process.promises = new Object()
-child_process.promises.execFile = util.promisify(child_process.execFile)
 
 async function main() {
     // Install extension
-    await child_process.promises.execFile('vsce', ['package', '-o', 'cpptheme.vsix'])
-    await child_process.promises.execFile('code', ['--install-extension', 'cpptheme.vsix'])
+    await _execFile('vsce', ['package', '-o', 'cpptheme.vsix'])
+    await _execFile('code', ['--install-extension', 'cpptheme.vsix'])
     await fs.promises.rm('cpptheme.vsix')
 }
+
+
+
+const _execFile = util.promisify(child_process.execFile)
+
+
 
 main()
--- upload.js
+++ upload.js
@@ -1,25 +1,30 @@
+// @ts-check
+
 const child_process = require('child_process')
 const fs            = require('fs')
 const util          = require('util')
-child_process.promises = new Object()
-child_process.promises.execFile = util.promisify(child_process.execFile)
 
 async function main() {
     // Git pull
-    child_process.execFile
-    await child_process.promises.execFile('git', ['pull'])
+    await _execFile('git', ['pull'])
 
     // Git commit
     try {
-        await child_process.promises.execFile('git', ['add', '.'])
-        await child_process.promises.execFile('git', ['commit', '-m', 'update'])
+        await _execFile('git', ['add', '.'])
+        await _execFile('git', ['commit', '-m', 'update'])
     } catch (_) { }
 
     // Vsce upload
-    await child_process.promises.execFile('vsce', ['publish', 'patch', '--pat', await fs.promises.readFile('vsce-token.txt')])
+    await _execFile('vsce', ['publish', 'patch', '--pat', (await fs.promises.readFile('vsce-token.txt')).toString()])
 
     // Git push
-    await child_process.promises.execFile('git', ['push'])
+    await _execFile('git', ['push'])
 }
+
+
+
+const _execFile = util.promisify(child_process.execFile)
+
+
 
 main()
