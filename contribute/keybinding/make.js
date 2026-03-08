9ac87fe81ca6565bf029c0fc0d050012ac4223ce
shyeyian <shyeyian@icloud.com>
2026-03-08 19:32:24 +0800

    update

--- contribute/keybinding/make.js
+++ contribute/keybinding/make.js
@@ -4,8 +4,8 @@ const fs   = require('fs')
 const path = require('path')
 
 async function make() {
-    const linuxFile  = path.join('contribute', 'keybinding', 'linux.update.json')
-    const darwinFile = path.join('contribute', 'keybinding', 'darwin.update.json')
+    const linuxFile  = path.join('contribute', 'keybinding', 'linux-update.json')
+    const darwinFile = path.join('contribute', 'keybinding', 'darwin-update.json')
     const linuxJson  = (await fs.promises.readFile(linuxFile)).toString()
     const darwinJson = linuxJson.replaceAll(/\bctrl\b/g, 'cmd')
     await fs.promises.writeFile(darwinFile, darwinJson)
