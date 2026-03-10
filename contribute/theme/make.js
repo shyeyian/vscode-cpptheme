916acf77960d0962f9865ea55a4622141af32852
shyeyian <shyeyian@icloud.com>
2026-03-11 02:19:59 +0800

    update

--- contribute/theme/make.js
+++ contribute/theme/make.js
@@ -6,16 +6,30 @@ const path = require('path')
 async function make() {
     // Update color/light.json
     const darkJson  = (await fs.promises.readFile(path.join('contribute', 'theme', 'cpptheme-dark.json'))).toString()
-    const lightJson = darkJson.replace(/#[0-9a-f]{6}/, color => {
-        return new Map([
-            ["#000000", "#ffffff"],
-            ["#202020", "#f0f0f0"],
-            ["#404040", "#e0e0e0"],
-            ["#606060", "#d0d0d0"],
-            ["#808080", "#c0c0c0"],
-            ["#ffffff", "#000000"]
-        ]).get(color) ?? color
-    })
+    const lightJson = darkJson
+        .replaceAll(/\bdark\b/g, 'light')
+        .replaceAll(/#[0-9a-f]{6}/g, darkColor => {
+            const lightColor = new Map([
+                ["#000000", "#ffffff"],
+                ["#202020", "#dfdfdf"],
+                ["#404040", "#bfbfbf"],
+                ["#606060", "#9f9f9f"],
+                ["#808080", "#7f7f7f"],
+                ["#ffffff", "#000000"],
+
+                ["#ff0000", "#ff0000"],
+                ["#ff8000", "#ff8000"],
+                ["#d0d000", "#d0d000"],
+                ["#00a000", "#00a000"],
+                ["#0080ff", "#0080ff"],
+                ["#c000ff", "#c000ff"],
+                ["#b4b4b4", "#000000"]
+            ]).get(darkColor)
+            if (lightColor != undefined)
+                return lightColor
+            else
+                throw new Error(`color ${darkColor} is not mapped`)
+        })
     await fs.promises.writeFile(path.join('contribute', 'theme', 'cpptheme-light.json'), lightJson)
 }
 
